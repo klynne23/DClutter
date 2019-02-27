472ac34bee89691344b8d146d1aa913d38fa3848
Kirthi Bisset <kirthi.bisset@yahoo.com>
2019-02-27 10:35:06 -0500

    slash instead of start

--- server.js
+++ server.js
@@ -9,10 +9,9 @@ app.use(express.json());
 
 // Static assets
 if (process.env.NODE_ENV === "production") {
-    console.log("inside production env");
     app.use(express.static("client/build"));
     const path = require('path');
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 };
